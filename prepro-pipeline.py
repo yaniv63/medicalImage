64ef623351e806ede452ffbc14cee8e5050d7645
yaniv <yanivshachor63@gmail.com>
2017-01-03 23:07:49 +0200

    backup

--- prepro-pipeline.py
+++ prepro-pipeline.py
@@ -16,8 +16,8 @@ import scipy.misc
 FLAIR_th = 0.91
 WM_prior_th = 0.5
 
-FLAIR_vol_filename = r"/home/yaniv/src/medicalImaging/person1_time1/Person01_Time01_FLAIR.npy"
-WM_filename = r"/home/yaniv/src/medicalImaging/person1_time1/Person01_Time01.npy"
+Src_Path = r"/media/sf_ubuntuFolder/src/medicalImaging/data/"
+
 
 
 # In[10]:
@@ -34,31 +34,26 @@ def binary_disk(r):
 
 
 # In[11]:
-
-FLAIR_vol = np.load(FLAIR_vol_filename)
-WM = np.load(WM_filename)
-
-# dilate WM mask
-sel = binary_disk(2)
-WM_dilated = mrph.filters.maximum_filter(WM, footprint=sel)
-
-# apply thresholds
-FLAIR_mask = FLAIR_vol > FLAIR_th
-WM_mask = WM_dilated > WM_prior_th
-
-# final mask: logical AND
-candidate_mask = np.logical_and(FLAIR_mask, WM_mask)
+def apply_masks(FLAIR_vol_filename,WM_filename):
+    FLAIR_vol = np.load(FLAIR_vol_filename)
+    WM = np.load(WM_filename)
+    
+    # dilate WM mask
+    sel = binary_disk(2)
+    WM_dilated = mrph.filters.maximum_filter(WM, footprint=sel)
+    
+    # apply thresholds
+    FLAIR_mask = FLAIR_vol > FLAIR_th
+    WM_mask = WM_dilated > WM_prior_th
+    
+    # final mask: logical AND
+    candidate_mask = np.logical_and(FLAIR_mask, WM_mask)
+    return candidate_mask
 
 import matplotlib
 import pylab
 
 z = 70
-#pylab.figure()
-#pylab.imshow(FLAIR_vol[z,:,:], cmap=matplotlib.cm.gray)
-#pylab.figure()
-#pylab.imshow(WM_dilated[z,:,:], cmap=matplotlib.cm.gray)
-#pylab.figure()
-#pylab.imshow(candidate_mask[z,:,:], cmap=matplotlib.cm.gray)
 
 #%% patching
 
@@ -119,57 +114,61 @@ zc = 100
 # In[6]:
 
 # load volume
-FLAIR_filename = r"/home/yaniv/src/medicalImaging/person1_time1/Person01_Time01_FLAIR.npy"
-FLAIR_labels_1 = r"/home/yaniv/src/medicalImaging/person1_time1/training01_01_mask1.nii"
-vol = np.load(FLAIR_filename)
-vol.shape
-labels = nb.load(FLAIR_labels_1).get_data()
+for i in range(2):
+    index = i+1
+    Person = "person0%d"%(index)
+    FLAIR_filename = Src_Path+Person+"/"+Person+"_Time01_FLAIR.npy"
+    WM_filename = Src_Path+Person+"/"+Person+"_Time01.npy"    
+    FLAIR_labels_1 = Src_Path+Person+"/"+"training0%d_01_mask1.nii"%(index)
+    vol = np.load(FLAIR_filename)
+    labels = nb.load(FLAIR_labels_1).get_data()
+    
+    # In[7]:
+    
+    # initialize interpolator
+    x = np.linspace(0, vol.shape[2]-1,vol.shape[2])
+    y = np.linspace(0, vol.shape[1]-1,vol.shape[1])
+    z = np.linspace(0, vol.shape[0]-1,vol.shape[0])
+    interp3 = RegularGridInterpolator((z,y,x), vol)
+#%%
+    candidate_mask = apply_masks(FLAIR_filename,WM_filename)    
 
-# In[7]:
-
-# initialize interpolator
-x = np.linspace(0, vol.shape[2]-1,vol.shape[2])
-y = np.linspace(0, vol.shape[1]-1,vol.shape[1])
-z = np.linspace(0, vol.shape[0]-1,vol.shape[0])
-interp3 = RegularGridInterpolator((z,y,x), vol)
-
-
-# In[12]:
-patches= {}
-patches_axial = []
-patches_coronal = []
-patches_labels = []
-voxel_list = itertools.product(x,y,z)
-zero_count = 0
-for i,j,k in voxel_list:
-    if candidate_mask[i][j][k] == True:
-        axial_p = extract_axial(interp3, i, j, k, sz, w)
-        coronal_p = extract_coronal(interp3, i, j, k, sz, w)
-        if type(axial_p) == np.ndarray and type(coronal_p) == np.ndarray:        
-            if labels[i][j][k] == 0 and zero_count < 1500:            
-                zero_count= zero_count+1                
-                patches_axial.append(axial_p)
-                patches_coronal.append(coronal_p)
-                patches_labels.append(labels[i][j][k])
-            elif  labels[i][j][k] == 1:
-                patches_axial.append(axial_p)
-                patches_coronal.append(coronal_p)
-                patches_labels.append(labels[i][j][k])
-    if len(patches_axial) > 3000:
-        break
-
-axial_p = extract_axial(interp3, 73, 101, 104, sz, w)
-coronal_p = extract_coronal(interp3, 73, 101, 104, sz, w)
-patches_axial.append(axial_p)
-patches_coronal.append(coronal_p)
-patches_labels.append(labels[i][j][k])
-
-import pickle
-
-with open('patches_axial.lst', 'wb') as fp1 ,open('patches_coronal.lst', 'wb') as fp2,open('labels.lst', 'wb') as fp3 :
-    pickle.dump(patches_axial, fp1)
-    pickle.dump(patches_coronal, fp2)
-    pickle.dump(patches_labels, fp3)
+    # In[12]:
+    patches= {}
+    patches_axial = []
+    patches_coronal = []
+    patches_labels = []
+    voxel_list = itertools.product(x,y,z)
+    zero_count = 0
+    for i,j,k in voxel_list:
+        if candidate_mask[i][j][k] == True:
+            axial_p = extract_axial(interp3, i, j, k, sz, w)
+            coronal_p = extract_coronal(interp3, i, j, k, sz, w)
+            if type(axial_p) == np.ndarray and type(coronal_p) == np.ndarray:        
+                if labels[i][j][k] == 0 and zero_count < 1500:            
+                    zero_count= zero_count+1                
+                    patches_axial.append(axial_p)
+                    patches_coronal.append(coronal_p)
+                    patches_labels.append(labels[i][j][k])
+                elif  labels[i][j][k] == 1:
+                    patches_axial.append(axial_p)
+                    patches_coronal.append(coronal_p)
+                    patches_labels.append(labels[i][j][k])
+        if len(patches_axial) > 3000:
+            break
+    
+    axial_p = extract_axial(interp3, 73, 101, 104, sz, w)
+    coronal_p = extract_coronal(interp3, 73, 101, 104, sz, w)
+    patches_axial.append(axial_p)
+    patches_coronal.append(coronal_p)
+    patches_labels.append(labels[i][j][k])
+    
+    import pickle
+    
+    with open('patches_axial_0%d.lst'%(index), 'wb') as fp1 ,open('patches_coronal_0%d.lst'%(index), 'wb') as fp2,open('labels_0%d.lst'%(index), 'wb') as fp3 :
+        pickle.dump(patches_axial, fp1)
+        pickle.dump(patches_coronal, fp2)
+        pickle.dump(patches_labels, fp3)
 
 
 #%%
--- two_predictors_combained.py
+++ two_predictors_combained.py
@@ -4,7 +4,7 @@ Created on Mon Dec 26 16:42:11 2016
 
 @author: yaniv
 """
-from model_atom_base import atom_model_conv,create_smodel
+from model_atom_base import create_smodel
 from keras.layers import Merge,Dense,Input,merge
 from keras.models import Model,Sequential
 import numpy as np
@@ -44,40 +44,48 @@ def two_parameters_combained_model():
     return model
 
 if __name__ == "__main__":          
-    weight_path = r'/home/yaniv/src/medicalImaging/weights_predictor'    
-    with open('patches_axial.lst', 'rb') as fp1 ,open('patches_coronal.lst', 'rb') as fp2,open('labels.lst', 'rb') as fp3 :
-        axial_samples = np.array(pickle.load(fp1))
-        coronal_samples = np.array(pickle.load(fp2))
-        labels_samples =  np.array(pickle.load(fp3))
+    weight_path = r'/home/yaniv/src/data/weights_predictor'    
+    with open('patches_axial_01.lst', 'rb') as fp1 ,open('patches_coronal_01.lst', 'rb') as fp2,open('labels_01.lst', 'rb') as fp3 :
+        axial_samples_train = np.array(pickle.load(fp1))
+        coronal_samples_train = np.array(pickle.load(fp2))
+        labels_samples_train =  np.array(pickle.load(fp3))
+    
+    with open('patches_axial_02.lst', 'rb') as fp1 ,open('patches_coronal_02.lst', 'rb') as fp2,open('labels_02.lst', 'rb') as fp3 :
+        axial_samples_test = np.array(pickle.load(fp1))
+        coronal_samples_test = np.array(pickle.load(fp2))
+        labels_samples_test =  np.array(pickle.load(fp3))
 
-    axial_samples=np.expand_dims(axial_samples,1)
-    coronal_samples=np.expand_dims(coronal_samples,1)
-    labels_samples=np.expand_dims(labels_samples,1)
+    axial_samples_train=np.expand_dims(axial_samples_train,1)
+    coronal_samples_train=np.expand_dims(coronal_samples_train,1)
+    labels_samples_train=np.expand_dims(labels_samples_train,1)
+    
+    axial_samples_test=np.expand_dims(axial_samples_test,1)
+    coronal_samples_test=np.expand_dims(coronal_samples_test,1)
+    labels_samples_test=np.expand_dims(labels_samples_test,1)
     
     #permute data    
-    permute = np.random.permutation(len(coronal_samples))
-    axial_samples = axial_samples[permute]
-    coronal_samples = coronal_samples[permute]
-    labels_samples = labels_samples[permute]
+    permute = np.random.permutation(len(axial_samples_train))
+    axial_samples_train = axial_samples_train[permute]
+    coronal_samples_train = coronal_samples_train[permute]
+    labels_samples_train = labels_samples_train[permute]
     #divide train-test
-    train_samples = [axial_samples[permute[ : int(len(permute) * .75)]],coronal_samples[permute[ : int(len(permute) * .75)]] ]    
-    train_labels = [labels_samples[permute[ : int(len(permute) * .75)]],labels_samples[permute[ : int(len(permute) * .75)]]]    
-    test_samples = [axial_samples[permute[ int(len(permute) * .75):]],coronal_samples[permute[ int(len(permute) * .75):]]]     
-    test_labels = [labels_samples[permute[ int(len(permute) * .75):]],labels_samples[permute[ int(len(permute) * .75):]]]    
+#    train_samples = [axial_samples[permute[ : int(len(permute) * .75)]],coronal_samples[permute[ : int(len(permute) * .75)]] ]    
+#    train_labels = [labels_samples[permute[ : int(len(permute) * .75)]],labels_samples[permute[ : int(len(permute) * .75)]]]    
+#    test_samples = [axial_samples[permute[ int(len(permute) * .75):]],coronal_samples[permute[ int(len(permute) * .75):]]]     
+#    test_labels = [labels_samples[permute[ int(len(permute) * .75):]],labels_samples[permute[ int(len(permute) * .75):]]]  
+    train_samples = [axial_samples_train,coronal_samples_train]    
+    train_labels = [labels_samples_train,labels_samples_train]    
+    test_samples = [axial_samples_test,coronal_samples_test]     
+    test_labels = [labels_samples_test,labels_samples_test]    
 
-#  sample_patches_filename = r"/home/yaniv/src/medicalImaging/ref/sample_patches.npz"
-#    Npz = np.load(sample_patches_filename)
-#    pos_curr = Npz['IPosCurr']
-#    neg_curr = Npz['INegCurr']
-#    flair_samples = np.concatenate((pos_curr[:,0,:,:,1],neg_curr[:,0,:,:,1])).reshape(100,1,32,32)
-#    flair_labels = np.concatenate((np.ones(flair_samples.shape[0]/2),np.zeros(flair_samples.shape[0]/2))) 
-#    T2_samples = np.concatenate((pos_curr[:,1,:,:,1],neg_curr[:,1,:,:,1])).reshape(100,1,32,32)
-#    T2_labels = np.concatenate((np.ones(flair_samples.shape[0]/2),np.zeros(flair_samples.shape[0]/2)))
+
     predictors = []
+    results = []
     for i in range(2):
         predictors.append(one_predictor_model(index=i))
         predictors[i].compile(optimizer='rmsprop',loss='binary_crossentropy',metrics=['accuracy'])
         predictors[i].fit(train_samples[i], train_labels[i],batch_size=300,shuffle=True)
+        results.append(predictors[i].evaluate(test_samples[i],test_labels[i]))
         predictors[i].save_weights(weight_path+'%d.h5'%(i))
     combain_predict = True
     if combain_predict ==True:
