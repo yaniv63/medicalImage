99ab94ede1b7169940c64bc4d794bf389342f520
yaniv <yanivshachor63@gmail.com>
2017-04-16 17:26:55 +0300

    test smodel save segmantation

--- test_smodel.py
+++ test_smodel.py
@@ -329,5 +329,6 @@ for i in range(4):
     #test(predictors[i],"axial",[5],i)
     #probability_plot(predictors[i],vol,i,slice=80,threshold=0.8)
     seg = predict_image(predictors[0],vol)
-
+    with open(run_dir + 'segmantation.lst', 'wb') as fp:
+        pickle.dump(seg, fp)
 
