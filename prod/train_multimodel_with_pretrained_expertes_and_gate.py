f244eb71741ee7823527aa7d979c96378799b0d4
Ubuntu <ubuntu@ip-172-31-41-54.us-west-2.compute.internal>
2017-09-17 14:51:29 +0000

    server side technical changes

--- prod/train_multimodel_with_pretrained_expertes_and_gate.py
+++ prod/train_multimodel_with_pretrained_expertes_and_gate.py
@@ -7,7 +7,7 @@ Created on Wed Dec 21 19:32:39 2016
 # create logger
 
 from paths import *
-from prod.logging_tools import get_logger
+from logging_tools import get_logger
 
 run_dir = get_run_dir()
 logger = get_logger(run_dir)
@@ -19,7 +19,7 @@ from sklearn.model_selection import KFold
 import sys
 
 
-from prod.multi_predictors_combined import one_predictor_model,n_predictors_combined_model,n_parameters_combined_model,n_experts_combined_model,n_experts_combined_model_gate_parameters
+from multi_predictors_combined import one_predictor_model,n_predictors_combined_model,n_parameters_combined_model,n_experts_combined_model,n_experts_combined_model_gate_parameters
 from train_tools import create_callbacks,generator,combined_generator,aggregate_genrated_samples\
     , calc_epoch_size,combined_aggregate_genrated_samples
 from data_containers import load_data,load_all_data
@@ -27,24 +27,6 @@ from metrics import calc_confusion_mat,calc_dice
 from plotting_tools import *
 from train_proccesses import TrainGenerator
 
-def train(model,PersonTrainList,PersonValList,view_type,contrast_type,fold_num,name,batch_size=256):
-    logger.debug("creating callbacks")
-    callbacks = create_callbacks(name,fold_num)
-
-    logger.debug("creating train & val generators")
-
-    train_images,pos_train_list,neg_train_list = load_data(PersonTrainList,contrast_type)
-    train_generator = generator(pos_train_list, neg_train_list, train_images,view_type)
-    val_images,pos_val_list,neg_val_list = load_data(PersonValList,contrast_type)
-    val_set = aggregate_genrated_samples(pos_val_list, neg_val_list, val_images,view_type)
-
-    logger.info("training individual model")
-    epoch_size = calc_epoch_size(pos_train_list,batch_size)
-    history = model.fit_generator(train_generator, samples_per_epoch=epoch_size, nb_epoch=300, callbacks=callbacks,
-                                      validation_data=val_set)
-    confusion_mat = calc_confusion_mat(model, val_set[0], val_set[1], "individual val {}".format(fold_num))
-    calc_dice(confusion_mat, "individual val {}".format(fold_num))
-    return history
 
 def train_combined(model,PersonTrainList,PersonValList,contrast_list,view_list,name,batch_size=256):
 
@@ -59,7 +41,7 @@ def train_combined(model,PersonTrainList,PersonValList,contrast_list,view_list,n
     epoch_size = calc_epoch_size(positive_list, batch_size)
     val_size = calc_epoch_size(pos_val_list, batch_size)
     gen = train_generator.get_generator()
-    history = model.fit_generator(gen, samples_per_epoch=epoch_size, nb_epoch=200, callbacks=callbacks,
+    history = model.fit_generator(gen, samples_per_epoch=epoch_size, nb_epoch=1, callbacks=callbacks,
                                   validation_data=val_set,nb_val_samples=val_size)
     gen.close()
     train_generator.close()
@@ -78,7 +60,7 @@ logger.debug("start script")
 MR_modalities = ['FLAIR', 'T2', 'MPRAGE', 'PD']
 view_list = ['axial','coronal', 'sagittal']
 
-
+station = 'server'
 data = np.array([[(1,x) for x in range(1,5)],[(2,x) for x in range(1,5)],[(3,x) for x in range(1,6)],[(4,x) for x in range(1,5)],
         [(5,x) for x in range(1,5)]])
 kf = KFold(n_splits=5)
@@ -94,10 +76,13 @@ for train_index, test_index in kf.split(data):
     logger.info("TRAIN: {} VAL: {} , TEST: {}".format(train_d,val_d,test_person))
 
     name="test_{}".format(test_person)
-    logger.info("training model {}".format(name))
+    logger.info("training model {} . multimodel with pretrained expertes and gate. use parameters gate".format(name))
     runs = []
-    w_path = '/media/sf_shared/src/medicalImaging/runs/MOE runs/run5-moe with pretrained experts/'
-    w_path_gate = '/media/sf_shared/src/medicalImaging/runs/MOE runs/run9-pretrain gate parameters/'
+    if station=='desktop':
+	w_path =  '/media/sf_shared/src/medicalImaging/runs/MOE runs/run5-moe with pretrained experts/'
+        w_path_gate = '/media/sf_shared/src/medicalImaging/runs/MOE runs/run9-pretrain gate parameters/'
+    else:
+	w_path = weight_path + 'moe/'
 
     moe = n_experts_combined_model_gate_parameters(n=3, N_mod=4, img_rows=33, img_cols=33)
     moe.get_layer('Seq_0').load_weights(w_path + 'model_test_1_axial_fold_0.h5', by_name=True)
@@ -109,8 +94,8 @@ for train_index, test_index in kf.split(data):
     moe.get_layer('Seq_2').load_weights(w_path + 'model_test_1_sagittal_fold_0.h5', by_name=True)
     moe.load_weights(w_path + 'model_test_1_sagittal_fold_0.h5', by_name=True)
 
-    moe.load_weights(w_path_gate + 'gate_parameters_test1.h5',by_name=True)
-
+    moe.load_weights(w_path + 'gate_parameters_test1.h5',by_name=True)
+    #moe.load_weights(w_path + 'w_test1.h5')
 
     optimizer = SGD(lr=0.01, nesterov=True)
     moe.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy', 'fmeasure'])
